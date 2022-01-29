071332f6357b490b925cd689595eea06f5450f78
amanmathurastro <amanmathurastro@gmail.com>
2022-01-30 02:07:07 +0530

    Addition of Loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loader.scss";
+
+const Loader = () => {
+  return (
+    <div className="pos-center">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Loader;
